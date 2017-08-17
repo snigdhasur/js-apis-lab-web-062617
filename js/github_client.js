2f913ce7ab03c768be84b8ea3bdb51e6d46d8a85
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-08-17 11:45:36 -0400

    fixed myGists

--- js/github_client.js
+++ js/github_client.js
@@ -1,9 +1,39 @@
-//define functions here
 var createGist = function(file_name, content, description, token){
+
+	var dataObject = Object.assign({}, {
+        'public':   true,
+        'description': "",
+        'files': {}
+        }
+       )
+
+	dataObject.description = description
+	dataObject.files[file_name] = {}
+	dataObject.files[file_name].content = content 
+
+
+	$.ajax({
+	  url: 'https://api.github.com/gists',
+	  type: 'POST',
+	  dataType: 'json',
+	  headers: {
+	    Authorization: "token " + token
+	  },
+	  data: JSON.stringify(dataObject)
+	})
+
 
 };
 
 var myGists = function (username, token){
+	$.ajax({
+	  url: 'https://api.github.com/users/' + username + '/gists',
+	  type: 'GET',
+	  dataType: 'json',
+	  headers: {
+	    Authorization: "token " + token
+	  }
+	})
 
 };
 
@@ -13,4 +43,13 @@ var bindCreateButton = function() {
 };
 
 $(document).ready(function(){
+	document.getElementById("submit").addEventListener("click", function(e){
+		e.preventDefault()
+		var fileName = document.getElementById("file_name")
+		var content = document.getElementById("content")
+		var token = document.getElementById("token")
+		var description = document.getElementById("description")
+		console.log("I was clicked!")
+		createGist(fileName, content, description, token)
+	})
 });
